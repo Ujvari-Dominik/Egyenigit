e986b2a2a540d0d703ab0bdf414245fde9d7bf8a
Dominik <ujvari.dominik0021@gmail.com>
2023-03-09 09:26:27 +0100

    uj class uzentetel

--- src/main/java/org/example/Hellobranch.java
+++ src/main/java/org/example/Hellobranch.java
@@ -3,5 +3,6 @@ package org.example;
 public class Hellobranch {
     public static void main(String[] args) {
         System.out.println("Hello új branch");
+        System.out.println("új próbák");
     }
 }
